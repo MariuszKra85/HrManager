f82202a0c2ee4cea2d4b8fb31e525d1b07670d21
Mariusz Krawczyk <przyslony@gmail.com>
2020-10-04 21:05:41 +0100

    create NewEmployee file, create new branch

---
+++ src/Component/Employees/NewEmployee.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class NewEmployee extends Component {
+  state = {
+    name: '',
+    surname: '',
+    empNumber: '',
+    startDate: '',
+    position: '',
+  };
+  render() {
+    return <div></div>;
+  }
+}
+
+export default NewEmployee;
